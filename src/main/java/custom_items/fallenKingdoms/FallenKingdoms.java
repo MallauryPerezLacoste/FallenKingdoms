79ae7d374ac00444a4be5e14e2e32e1dba4472fd
MallauryPerezLacoste <perezmallaury@gmail.com>
2024-08-23 11:26:36 +0200

    Initial commit

---
+++ src/main/java/custom_items/fallenKingdoms/FallenKingdoms.java
@@ -0,0 +1,17 @@
+package custom_items.fallenKingdoms;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class FallenKingdoms extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
